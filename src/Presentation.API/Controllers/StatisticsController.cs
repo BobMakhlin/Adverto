852b415428081095570710b4926e20475baf273c
Bob Makhlin <borismakhlin5@gmail.com>
2021-07-30 21:12:00 +0300

    Add the postfix 'Async' to each action method of StatisticsController

--- src/Presentation.API/Controllers/StatisticsController.cs
+++ src/Presentation.API/Controllers/StatisticsController.cs
@@ -15,28 +15,28 @@ namespace Presentation.API.Controllers
     public class StatisticsController : MyBaseController
     {
         [HttpGet("views-of-each-adtype")]
-        public async Task<ActionResult<Dictionary<AdType, int>>> GetViewsOfEachAdType()
+        public async Task<ActionResult<Dictionary<AdType, int>>> GetViewsOfEachAdTypeAsync()
         {
             Dictionary<AdType, int> viewsOfEachAdType = await Mediator.Send(new GetViewsOfEachAdTypeQuery());
             return Ok(viewsOfEachAdType);
         }
 
         [HttpGet("top3-ad-categories")]
-        public async Task<ActionResult<List<CategoryDto>>> GetTop3AdCategories()
+        public async Task<ActionResult<List<CategoryDto>>> GetTop3AdCategoriesAsync()
         {
             List<CategoryDto> top3AdCategories = await Mediator.Send(new GetTop3AdCategoriesQuery());
             return Ok(top3AdCategories);
         }
 
         [HttpGet("top10-ads-by-views")]
-        public async Task<ActionResult<List<AdDto>>> GetTop10AdsByViews()
+        public async Task<ActionResult<List<AdDto>>> GetTop10AdsByViewsAsync()
         {
             List<AdDto> top10AdsByViews = await Mediator.Send(new GetTop10AdsByViewsQuery());
             return Ok(top10AdsByViews);
         }
         
         [HttpGet("top15-tags-by-views")]
-        public async Task<ActionResult<List<TagDto>>> GetTop15TagsByViews()
+        public async Task<ActionResult<List<TagDto>>> GetTop15TagsByViewsAsync()
         {
             List<TagDto> top15TagsByViews = await Mediator.Send(new GetTop15TagsByViewsQuery());
             return Ok(top15TagsByViews);
