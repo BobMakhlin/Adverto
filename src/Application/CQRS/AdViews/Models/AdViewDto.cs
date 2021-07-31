4d2331d2de72ca4d8040317a2fb830725a5b8e75
Bob Makhlin <borismakhlin5@gmail.com>
2021-07-31 08:48:32 +0300

    Include related entities when mapping Ad to AdDto
    Now AdDto has a collection of Categories, Tags, Views and a property IsDisabled.

---
+++ src/Application/CQRS/AdViews/Models/AdViewDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Application.Common.Mappings.Interfaces;
+using Domain.Primary.Entities;
+
+namespace Application.CQRS.AdViews.Models
+{
+    public class AdViewDto : IMapFrom<AdView>
+    {
+        #region Properties
+
+        public DateTime ViewedAt { get; set; }
+
+        #endregion
+    }
+}
--- src/Application/CQRS/Ads/Models/AdDto.cs
+++ src/Application/CQRS/Ads/Models/AdDto.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using Application.Common.Mappings.Interfaces;
+using Application.CQRS.AdViews.Models;
+using Application.CQRS.Categories.Model;
+using Application.CQRS.Tags.Models;
+using AutoMapper;
 using Domain.Primary.Entities;
 
 namespace Application.CQRS.Ads.Models
@@ -12,6 +17,28 @@ namespace Application.CQRS.Ads.Models
         public AdType AdType { get; set; }
         public double Cost { get; set; }
         public string Content { get; set; }
+        public List<TagDto> Tags { get; set; }
+        public List<CategoryDto> Categories { get; set; }
+        public List<AdViewDto> AdViews { get; set; }
+        public bool IsDisabled { get; set; }
+
+        #endregion
+
+        #region IMapFrom<Ad>
+
+        public void MapUsingProfile(Profile profile)
+        {
+            profile.CreateMap<Ad, AdDto>()
+                .ForMember
+                (
+                    adDto => adDto.IsDisabled,
+                    opts => opts.MapFrom(ad => ad.DisabledAd != null)
+                );
+
+            profile.CreateMap<Tag, TagDto>();
+            profile.CreateMap<Category, CategoryDto>();
+            profile.CreateMap<AdView, AdViewDto>();
+        }
 
         #endregion
     }
