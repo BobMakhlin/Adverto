bc27129734ea9b071b7f49185751ba8c69983ece
Bob Makhlin <borismakhlin5@gmail.com>
2021-08-01 09:36:07 +0300

    Fix #4

--- src/Application/CQRS/Statistics/Queries/GetViewsOfEachAdTypeQuery.cs
+++ src/Application/CQRS/Statistics/Queries/GetViewsOfEachAdTypeQuery.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -35,6 +36,34 @@ namespace Application.CQRS.Statistics.Queries
             public async Task<Dictionary<AdType, int>> Handle(GetViewsOfEachAdTypeQuery request,
                 CancellationToken cancellationToken)
             {
+                Dictionary<AdType, int> viewsGroupedByAdTypes =
+                    await GetViewsOfEachAdTypeViewedAtLeastOnceAsync(cancellationToken)
+                        .ConfigureAwait(false);
+
+                IEnumerable<AdType> allExistingAdTypes = GetAllExistingAdTypes();
+                IEnumerable<AdType> usedAdTypes = viewsGroupedByAdTypes.Select(x => x.Key);
+                IEnumerable<AdType> unusedAdTypes = allExistingAdTypes.Except(usedAdTypes);
+
+                foreach (AdType unusedAdType in unusedAdTypes)
+                {
+                    int countOfViews = 0;
+                    viewsGroupedByAdTypes.Add(unusedAdType, countOfViews);
+                }
+
+                return viewsGroupedByAdTypes;
+            }
+
+            #endregion
+
+            #region Methods
+
+            /// <summary>
+            /// Returns the dictionary where the key is <see cref="AdType"/> and the value is views count.
+            /// Note that this dictionary doesn't contain the ad-types, having zero views.
+            /// </summary>
+            private async Task<Dictionary<AdType, int>> GetViewsOfEachAdTypeViewedAtLeastOnceAsync(
+                CancellationToken cancellationToken)
+            {
                 return await _context.AdViews
                     .AsNoTracking()
                     .Include(adView => adView.Ad)
@@ -52,6 +81,11 @@ namespace Application.CQRS.Statistics.Queries
                     )
                     .ConfigureAwait(false);
             }
+            
+            /// <summary>
+            /// Returns the collection of all existing ad-types.
+            /// </summary>
+            private IEnumerable<AdType> GetAllExistingAdTypes() => (AdType[]) Enum.GetValues(typeof(AdType));
 
             #endregion
         }
