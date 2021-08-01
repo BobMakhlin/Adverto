3d3f3bb12ddfc392dc11e2380cab60849afe104d
Bob Makhlin <borismakhlin5@gmail.com>
2021-08-01 20:03:11 +0300

    Check if ad has more than one category when removing category of the ad
    There is a rule that the ad must have at least one category. When removing the category of the ad, we need to check if this ad has more than one category.
    We made this via FluentValidation.

--- src/Application.Validation/AbstractValidators/CQRS/Ads/Commands/AdCategory/DeleteCategoryOfAdCommandValidator.cs
+++ src/Application.Validation/AbstractValidators/CQRS/Ads/Commands/AdCategory/DeleteCategoryOfAdCommandValidator.cs
@@ -1,17 +1,54 @@
-﻿using Application.CQRS.Ads.Commands.AdCategory;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.CQRS.Ads.Commands.AdCategory;
+using Application.Persistence.Interfaces;
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Validation.AbstractValidators.CQRS.Ads.Commands.AdCategory
 {
     public class DeleteCategoryOfAdCommandValidator : AbstractValidator<DeleteCategoryOfAdCommand>
     {
-        public DeleteCategoryOfAdCommandValidator()
+        #region Fields
+
+        private readonly IAdvertoDbContext _context;
+
+        #endregion
+        
+        #region Constructors
+
+        public DeleteCategoryOfAdCommandValidator(IAdvertoDbContext context)
         {
-            RuleFor(c => c.AdId)
-                .NotEmpty();
+            _context = context;
             
+            RuleFor(c => c.AdId)
+                .NotEmpty()
+                .MustAsync(HaveMoreThanOneCategory)
+                .WithMessage
+                    ("You cannot remove the last category of the ad. The ad must have at least one category.");
+
             RuleFor(c => c.CategoryId)
                 .NotEmpty();
         }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Checks if the ad with the specified <paramref name="adId"/> has more than one category.
+        /// </summary>
+        private async Task<bool> HaveMoreThanOneCategory(Guid adId, CancellationToken token)
+        {
+            int adCategoriesCount = await _context.Ads
+                .Where(ad => ad.AdId == adId)
+                .Select(ad => ad.Categories.Count())
+                .SingleOrDefaultAsync(token);
+            return adCategoriesCount > 1;
+        }
+
+        #endregion
     }
 }
